93b2618e8a74af1a51fe5e502fc918a2fa049ec1
Jakub Arnold <darthdeus@gmail.com>
2014-08-03 19:16:07 +0200

    Everything works as if by magic

--- Parser.hs
+++ Parser.hs
@@ -13,19 +13,19 @@ pars :: Parser a -> Parser a
 pars p = try (char '(' *> p <* char ')') <|> p
 
 lambda :: Parser Expr
-lambda = pars $ do
+lambda = do
     binding <- char '\\' *> many1 letter <* char '.'
     body <- expr
     return $ Lam binding body
 
 var :: Parser Expr
-var = pars $ Var <$> many1 letter
+var = Var <$> many1 letter
 
 app :: Parser Expr
-app = pars $ chainl1 terminal (char ' ' *> return App)
+app = chainl1 terminal (char ' ' *> return App)
 
 terminal :: Parser Expr
-terminal = try var <|> lambda
+terminal = try var <|> lambda <|> pars expr
 
 expr :: Parser Expr
 expr = try app <|> try var <|> lambda
