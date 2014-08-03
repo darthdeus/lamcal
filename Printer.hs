bdf16df3dabfd28e16e6b591fb869c0dada66d46
Jakub Arnold <darthdeus@gmail.com>
2014-08-03 19:48:01 +0200

    Add basic printer specs

--- Printer.hs
+++ Printer.hs
@@ -1,3 +1,22 @@
 module Printer where
 
--- TODO - implement the actual printing logic
+import Types
+import Test.Hspec
+
+showExp :: Expr -> String
+showExp (Var x) = x
+showExp (Lam x y) = "\\" ++ x ++ "." ++ showExp y
+showExp (App x y) = show x ++ " " ++ show y
+
+(|@|) :: (Show a, Eq a) => a -> a -> Expectation
+(|@|) = shouldBe
+
+main :: IO ()
+main = hspec $ do
+    it "var printer" $ do
+        showExp (Var "x") |@| "x"
+        showExp (Var "hello") |@| "hello"
+
+    it "lambda printer" $ do
+        showExp (Lam "x" (Var "x")) |@| "\\x.x"
+        showExp (Lam "x" (Lam "y" (Var "x"))) |@| "\\x.\\y.x"
