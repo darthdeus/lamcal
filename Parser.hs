e16c615f8d1d1d31d35650b35f7a8a026a3569bb
Jakub Arnold <darthdeus@gmail.com>
2014-08-03 18:53:40 +0200

    Incredible magic of `chainl1`

--- Parser.hs
+++ Parser.hs
@@ -6,6 +6,7 @@ import Main
 import Text.Parsec.Prim
 import Text.Parsec.Char
 import Text.Parsec.Combinator
+import Text.Parsec.Error
 import Text.Parsec.String
 
 pp :: Show a => Either a Expr -> IO ()
@@ -21,15 +22,21 @@ parseLambda = do
 parseVar :: Parser Expr
 parseVar = Var <$> many1 letter
 
+parseParenLambda :: Parser Expr
+parseParenLambda = char '(' *> parseLambda <* char ')'
+
+parseTerminal :: Parser Expr
+parseTerminal = try parseVar <|> parseParenLambda
+
 parseApp :: Parser Expr
 parseApp = do
-    f <- parseExpr
-    char ' '
-    x <- parseExpr
-    return $ App f x
+    let unpar = chainl1 parseTerminal (char ' ' *> return App)
+    let par = char '(' *> unpar <* char ')'
+    try par <|> unpar
 
 parseExpr :: Parser Expr
 parseExpr = do
     try parseLambda <|> try parseApp <|> parseVar
 
+test :: String -> Either ParseError Expr
 test = parse parseExpr "lambda calculus parser"
