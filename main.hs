bb7a27d08a6e908c580122146469b00e1eb053cd
Jakub Arnold <darthdeus@gmail.com>
2014-08-03 14:23:49 +0200

    Add basic parser combinators

--- main.hs
+++ main.hs
@@ -1,11 +1,40 @@
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE GADTs #-}
 module Main where
+
+import Control.Applicative hiding ((<|>))
+import Text.Parsec.Prim
+import Text.Parsec.Char
+import Text.Parsec.Combinator
+import Text.Parsec.String
 
 data Expr = Var String
           | Lam String Expr
           | App Expr Expr
+        deriving Show
+-- instance Show Expr where
+--     show (Var x) = x
+--     show (Lam x t) = "\\" ++ x ++ ". " ++ show t
+--     show (App f x) = "(" ++ show f ++ ") " ++ show x
 
-instance Show Expr where
-    show (Var x) = x
-    show (Lam x t) = "\\" ++ x ++ ". " ++ show t
-    show (App f x) = "(" ++ show f ++ ") " ++ show x
+
+parseLambda :: Parser Expr
+parseLambda = do
+    binding <- char '\\' *> many1 letter <* char '.'
+    body <- parseExpr
+    return $ Lam binding body
+
+parseVar :: Parser Expr
+parseVar = Var <$> many1 letter
+
+parseApp :: Parser Expr
+parseApp = do
+    f <- parseExpr <* char ' '
+    x <- parseExpr
+    return $ App f x
+
+
+parseExpr :: Parser Expr
+parseExpr = try parseApp <|> try parseLambda <|> parseVar
+
+test = parse parseExpr "lambda calculus parser"
