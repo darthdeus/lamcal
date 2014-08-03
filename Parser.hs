0f2835c72b428d073eed59e388b0d7ef604c82a2
Jakub Arnold <darthdeus@gmail.com>
2014-08-03 15:14:13 +0200

    Split out parser to a separate module

---
+++ Parser.hs
@@ -0,0 +1,30 @@
+module Parser where
+
+import Control.Applicative hiding ((<|>))
+
+import Main
+import Text.Parsec.Prim
+import Text.Parsec.Char
+import Text.Parsec.Combinator
+import Text.Parsec.String
+
+parseLambda :: Parser Expr
+parseLambda = do
+    binding <- char '\\' *> many1 letter <* char '.'
+    body <- parseExpr
+    return $ Lam binding body
+
+parseVar :: Parser Expr
+parseVar = Var <$> many1 letter
+
+parseApp :: Parser Expr
+parseApp = do
+    f <- parseVar
+    char ' '
+    x <- parseExpr
+    return $ App f x
+
+parseExpr :: Parser Expr
+parseExpr = try parseLambda <|> try parseApp <|> parseVar
+
+test = parse parseExpr "lambda calculus parser"
--- main.hs
+++ main.hs
@@ -1,13 +1,5 @@
-{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE GADTs #-}
 module Main where
-
-import Control.Applicative hiding ((<|>))
-import Text.Parsec.Prim
-import Text.Parsec.Char
-import Text.Parsec.Combinator
-import Text.Parsec.Error
-import Text.Parsec.String
 
 type Identifier = String
 
@@ -16,31 +8,10 @@ data Expr = Var Identifier
           | App Expr Expr
             deriving Show
 
-pp :: Either ParseError Expr -> IO ()
+pp :: Show a => Either a Expr -> IO ()
 pp (Left e) = print e
 pp (Right e) = putStrLn $ p e
   where
     p (Var x) = x
     p (Lam x t) = "\\" ++ x ++ "." ++ p t
     p (App f x) = "(" ++ p f ++ " " ++ p x ++ ")"
-
-parseLambda :: Parser Expr
-parseLambda = do
-    binding <- char '\\' *> many1 letter <* char '.'
-    body <- parseExpr
-    return $ Lam binding body
-
-parseVar :: Parser Expr
-parseVar = Var <$> many1 letter
-
-parseApp :: Parser Expr
-parseApp = do
-    f <- parseVar
-    char ' '
-    x <- parseExpr
-    return $ App f x
-
-parseExpr :: Parser Expr
-parseExpr = try parseLambda <|> try parseApp <|> parseVar
-
-test = parse parseExpr "lambda calculus parser"
