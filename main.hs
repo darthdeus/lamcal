cf3842f668ccae69e4b024ad20b9bce6b19b1a25
Jakub Arnold <darthdeus@gmail.com>
2014-08-03 19:40:35 +0200

    Remove church numerals from Main

--- main.hs
+++ main.hs
@@ -17,23 +17,3 @@ p :: Expr -> String
 p (Var x) = x
 p (Lam x t) = "\\" ++ x ++ "." ++ paren t
 p (App f x) = paren f ++ " " ++ paren x
-
-c0 :: Expr
-c0 = Lam "z" (Lam "s" (Var "z"))
-
-c1 :: Expr
-c1 = Lam "z" (Lam "s" (App (Var "s") (Var "z")))
-
-true :: Expr
-true = Lam "t" (Lam "f" (Var "t"))
-
-false :: Expr
-false = Lam "t" (Lam "f" (Var "f"))
-
-omega :: Expr
-omega = App f f where f = Lam "x" (App (Var "x") (Var "x"))
-
-pair, fst, snd :: Expr
-pair = Lam "x" (Lam "y" (Lam "p" (App (App (Var "p") (Var "x")) (Var "y")))) -- "\x y p. p x y"
-fst = true
-snd = false
