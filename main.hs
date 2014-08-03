4aa997ea248fb96b117bab9c23eee0e612fcf3f4
Jakub Arnold <darthdeus@gmail.com>
2014-08-03 15:11:42 +0200

    Improve parens + fix parser recursion

--- main.hs
+++ main.hs
@@ -6,16 +6,25 @@ import Control.Applicative hiding ((<|>))
 import Text.Parsec.Prim
 import Text.Parsec.Char
 import Text.Parsec.Combinator
+import Text.Parsec.Error
 import Text.Parsec.String
 
-data Expr = Var String
-          | Lam String Expr
+type Identifier = String
+
+data Expr = Var Identifier
+          | Lam Identifier Expr
           | App Expr Expr
-        deriving Show
+            deriving Show
+
 -- instance Show Expr where
---     show (Var x) = x
---     show (Lam x t) = "\\" ++ x ++ ". " ++ show t
---     show (App f x) = "(" ++ show f ++ ") " ++ show x
+pp :: Either ParseError Expr -> IO ()
+pp (Left e) = print e
+pp (Right e) = putStrLn $ p e
+  where
+    p (Var x) = x
+    p (Lam x t) = "\\" ++ x ++ "." ++ p t
+    p (App f x) = "(" ++ p f ++ " " ++ p x ++ ")"
+
 
 
 parseLambda :: Parser Expr
@@ -29,12 +38,12 @@ parseVar = Var <$> many1 letter
 
 parseApp :: Parser Expr
 parseApp = do
-    f <- parseExpr <* char ' '
+    f <- parseVar
+    char ' '
     x <- parseExpr
     return $ App f x
 
-
 parseExpr :: Parser Expr
-parseExpr = try parseApp <|> try parseLambda <|> parseVar
+parseExpr = try parseLambda <|> try parseApp <|> parseVar
 
 test = parse parseExpr "lambda calculus parser"
