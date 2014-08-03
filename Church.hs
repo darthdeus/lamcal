db22d847f81a1f666f03af8e6d4d2eba1e203326
Jakub Arnold <darthdeus@gmail.com>
2014-08-03 19:40:43 +0200

    Extract Church to a separate moduel

---
+++ Church.hs
@@ -0,0 +1,23 @@
+module Church where
+
+import Types
+
+c0 :: Expr
+c0 = Lam "z" (Lam "s" (Var "z"))
+
+c1 :: Expr
+c1 = Lam "z" (Lam "s" (App (Var "s") (Var "z")))
+
+true :: Expr
+true = Lam "t" (Lam "f" (Var "t"))
+
+false :: Expr
+false = Lam "t" (Lam "f" (Var "f"))
+
+omega :: Expr
+omega = App f f where f = Lam "x" (App (Var "x") (Var "x"))
+
+pair, fst, snd :: Expr
+pair = Lam "x" (Lam "y" (Lam "p" (App (App (Var "p") (Var "x")) (Var "y")))) -- "\x y p. p x y"
+fst = true
+snd = false
