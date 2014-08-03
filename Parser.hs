ab6860c442b53026f73363f89a6f7a31991f4881
Jakub Arnold <darthdeus@gmail.com>
2014-08-03 19:37:59 +0200

    Add basic specs

--- Parser.hs
+++ Parser.hs
@@ -8,12 +8,13 @@ import Text.Parsec.Char
 import Text.Parsec.Combinator
 import Text.Parsec.Error
 import Text.Parsec.String
+import Test.Hspec
 
 pars :: Parser a -> Parser a
 pars p = try (char '(' *> p <* char ')') <|> p
 
 lambda :: Parser Expr
-lambda = do
+lambda = pars $ do
     binding <- char '\\' *> many1 letter <* char '.'
     body <- expr
     return $ Lam binding body
@@ -28,7 +29,32 @@ terminal :: Parser Expr
 terminal = try var <|> lambda <|> pars expr
 
 expr :: Parser Expr
-expr = try app <|> lambda <|> var
+expr = try app <|> try var <|> lambda
 
 test :: String -> Either ParseError Expr
 test = parse expr "lambda calculus parser"
+
+testParser :: Parser a -> String -> Either ParseError a
+testParser p = parse p ""
+
+(|@|) :: (Show a, Show b, Eq b) => Either a b -> b -> IO ()
+(|@|) ex y = case ex of
+    Left x -> fail $ show x
+    Right x -> x `shouldBe` y
+
+main = hspec $ do
+    it "app" $ do
+        testParser app "f x" |@| App (Var "f") (Var "x")
+        testParser app "(f x) y" |@| App (App (Var "f") (Var "x")) (Var "y")
+        testParser app "f (x y)" |@| App (Var "f") (App (Var "x") (Var "y"))
+
+    it "lambda" $ do
+        testParser lambda "\\x.x" |@| Lam "x" (Var "x")
+        testParser lambda "\\x.(x)" |@| Lam "x" (Var "x")
+
+    it "var" $ do
+        testParser var "x" |@| Var "x"
+
+    it "expr" $ do
+        testParser expr "\\x.x" |@| Lam "x" (Var "x")
+        testParser expr "(\\x.(x))" |@| Lam "x" (Var "x")
--- main.hs
+++ main.hs
@@ -7,7 +7,7 @@ type Identifier = String
 data Expr = Var Identifier
           | Lam Identifier Expr
           | App Expr Expr
-            deriving Show
+            deriving (Eq, Show)
 
 pp = putStrLn . p
 
