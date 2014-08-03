b233675d890d1a462a912a406509cd7130a9c45a
Jakub Arnold <darthdeus@gmail.com>
2014-08-03 19:12:19 +0200

    Major cleanup

--- Parser.hs
+++ Parser.hs
@@ -2,41 +2,33 @@ module Parser where
 
 import Control.Applicative hiding ((<|>))
 
-import Main
+import Main (Expr(..))
 import Text.Parsec.Prim
 import Text.Parsec.Char
 import Text.Parsec.Combinator
 import Text.Parsec.Error
 import Text.Parsec.String
 
-pp :: Show a => Either a Expr -> IO ()
-pp (Left e) = print e
-pp (Right e) = putStrLn $ p e
+pars :: Parser a -> Parser a
+pars p = try (char '(' *> p <* char ')') <|> p
 
-parseLambda :: Parser Expr
-parseLambda = do
+lambda :: Parser Expr
+lambda = pars $ do
     binding <- char '\\' *> many1 letter <* char '.'
-    body <- parseExpr
+    body <- expr
     return $ Lam binding body
 
-parseVar :: Parser Expr
-parseVar = Var <$> many1 letter
+var :: Parser Expr
+var = pars $ Var <$> many1 letter
 
-parseParenLambda :: Parser Expr
-parseParenLambda = char '(' *> parseLambda <* char ')'
+app :: Parser Expr
+app = pars $ chainl1 terminal (char ' ' *> return App)
 
-parseTerminal :: Parser Expr
-parseTerminal = try parseVar <|> parseParenLambda
+terminal :: Parser Expr
+terminal = try var <|> lambda
 
-parseApp :: Parser Expr
-parseApp = do
-    let unpar = chainl1 parseTerminal (char ' ' *> return App)
-    let par = char '(' *> unpar <* char ')'
-    try par <|> unpar
-
-parseExpr :: Parser Expr
-parseExpr = do
-    try parseLambda <|> try parseApp <|> parseVar
+expr :: Parser Expr
+expr = try app <|> try var <|> lambda
 
 test :: String -> Either ParseError Expr
-test = parse parseExpr "lambda calculus parser"
+test = parse expr "lambda calculus parser"
